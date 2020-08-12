3d7be2e16fee37704fcc56cc6883febbe989dc38
MuneebOvais <muneebovaiskhan@yahoo.com>
2020-08-13 00:46:46 +0500

    Updates

---
+++ precache-manifest.b10055264cb40f4f64850cd039ab0aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61778a10a05ad7c7463b149f4a75993",
+    "url": "/Lights-out-game/index.html"
+  },
+  {
+    "revision": "9de3259b9426cf9f4629",
+    "url": "/Lights-out-game/static/css/main.ad8916fe.chunk.css"
+  },
+  {
+    "revision": "73a42077e2e10e6608c9",
+    "url": "/Lights-out-game/static/js/2.0f919881.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lights-out-game/static/js/2.0f919881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de3259b9426cf9f4629",
+    "url": "/Lights-out-game/static/js/main.51917fce.chunk.js"
+  },
+  {
+    "revision": "bb5169a76fc8dce876be",
+    "url": "/Lights-out-game/static/js/runtime-main.90cf7c8c.js"
+  }
+]);
